96d13d81b95f10769beb5045773d995518a1cf05
santoshrashinkargithub <santoshrashinkar0108@gmail.com>
2023-09-27 18:52:06 +0530

    files added

---
+++ Multiplication.java
@@ -0,0 +1,31 @@
+package com.practice.besic.java;
+
+public class Multiplication {
+	
+	public int multiplicationOfNumber(int i,int j){
+
+	System.out.println("multiplication method started...!!");
+	
+	int k = i*j;
+	
+	System.out.println("multiplication is :" +k);
+	
+	System.out.println("multiplication method ended...!!");
+	
+	return k;
+	
+	}
+
+	public static void main(String[] args) {
+	
+		System.out.println("main method started...!!");
+		
+		Multiplication obj = new Multiplication();
+		
+		obj.multiplicationOfNumber(12, 9);
+	
+		System.out.println("main method ended...!!");
+
+	}
+
+}
